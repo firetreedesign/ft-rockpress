1092088f1b5117a5b455747840b6788a36b03c51
Daniel Milner <dmilner@gmail.com>
2021-07-20 08:45:44 -0700

    Added compatibility with WordPress 5.8

--- includes/class-rockpress-blocks.php
+++ includes/class-rockpress-blocks.php
@@ -1,51 +1,75 @@
 <?php
 /**
  * Blocks Initializer
- * 
+ *
  * Enqueue CSS/JS of all the blocks.
- * 
+ *
  * @since 1.0.11
  * @package RockPress
  */
 
- // Exit if accessed directly.
- if ( ! defined( 'ABSPATH' ) ) {
-     exit;
- }
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
- /**
-  * RockPress Blocks class
-  */
+/**
+ * RockPress Blocks class
+ */
 class RockPress_Blocks {
 
-    /**
-     * Initialize the class
-     * 
-     * @since 1.0.11
-     * 
-     * @return void
-     */
-    public static function init() {
-        add_filter( 'block_categories', __CLASS__ . '::block_categories', 10, 2 );
-    }
+	/**
+	 * Initialize the class
+	 *
+	 * @since 1.0.11
+	 *
+	 * @return void
+	 */
+	public static function init() {
+		if ( version_compare( $GLOBALS['wp_version'], '5.8alpha', '>=' ) ) {
+			add_filter( 'block_categories_all', __CLASS__ . '::block_categories_all', 10, 2 );
+		} else {
+			add_filter( 'block_categories', __CLASS__ . '::block_categories', 10, 2 );
+		}
+	}
 
-    /**
-     * Register a new block category for RockPress.
-     * 
-     * @since 1.0.11
-     * 
-     * @return void
-     */
-    public static function block_categories( $categories, $post ) {
-        return array_merge(
-            $categories,
-            array(
-                array(
-                    'slug' => 'rockpress',
-                    'title' => __( 'RockPress', 'ft-rockpress' ),
-                ),
-            )
-        );
-    }
+	/**
+	 * Register a new block category for RockPress.
+	 *
+	 * @since 1.0.11
+	 *
+	 * @return array
+	 */
+	public static function block_categories( $categories, $post ) {
+		return array_merge(
+			$categories,
+			array(
+				array(
+					'slug' => 'rockpress',
+					'title' => __( 'RockPress', 'ft-rockpress' ),
+				),
+			)
+		);
+	}
+
+	/**
+	 * Register a new block category for RockPress.
+	 *
+	 * @since 1.0.17
+	 * @param array  $block_categories Block categories.
+	 * @param object $editor_context Editor context.
+	 * @return array
+	 */
+	public static function block_categories_all( $block_categories, $editor_context ) {
+		array_push(
+			$block_categories,
+			array(
+				'slug'  => 'rockpress',
+				'title' => __( 'RockPress', 'ft-rockpress' ),
+				'icon'  => null,
+			)
+		);
+		return $block_categories;
+	}
 }
 RockPress_Blocks::init();
